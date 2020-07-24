9ab20e73241895164203883052d9336aa197e22b
BrianCL <briancardona87@gmail.com>
2020-07-24 17:39:11 -0500

    se organiza el html de la vista welcome con el fin, estoy validando cual pueda ser la causa que la pagina cargue sin estilos cuando se carga desde heroku y en celular

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,14 @@
 
 @section('content')
 <div class="container">
-<div class="row justify-content-center">
-    <div class="col-md-8 text-center mt-5">
-        <div class="display-4 pb-3">
-            Appgenda
-        </div>
-        <div class="h4 alert alert-secondary" role="alert">Programa tu cita facilmente con nuestra app, no gastes mas tiempo preguntando por fechas y horas disponibles, aquí lo tienes todo. Ingresa si tienes una cuenta o registrate para disfrutar de esta gran utilidad.</div>
-
-
-        <div class="links">
-           @auth
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center mt-5">
+            <div class="display-4 pb-3">
+                Appgenda
+            </div>
+            <div class="h4 alert alert-secondary" role="alert">Programa tu cita facilmente con nuestra app, no gastes mas tiempo preguntando por fechas y horas disponibles, aquí lo tienes todo. Ingresa si tienes una cuenta o registrate para disfrutar de esta gran utilidad.</div>
+            
+            @auth
                 <a class="btn btn-primary" href="{{ url('/home') }}">Home</a>
             @else
                 <a class="btn btn-primary" href="{{ route('login') }}">Ingreso</a>
@@ -22,6 +20,5 @@
             @endauth
         </div>
     </div>
-</div>
 </div>
 @endsection
